065c3ed4cf89ec3b23d97e076438831b1520840f
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-24 18:15:14 +0500

    Create a method for shrinking Polygons

---
+++ src/main/kotlin/org/tendiwa/plane/geometry/polygons/shrinking/PolygonShrinking.kt
@@ -0,0 +1,7 @@
+package org.tendiwa.plane.geometry.polygons.shrinking
+
+import org.tendiwa.geometry.polygons.Polygon
+import org.tendiwa.plane.geometry.straightSkeleton.StraightSkeleton
+
+fun Polygon.shrink(amount: Double) : Set<Polygon> =
+    StraightSkeleton(this).cap(amount)
